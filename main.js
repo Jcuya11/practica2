3acfadfab6823727dd4775c747d1fd0af92a4d0b
Alumno10 J901 <a10j901@campustls.local>
2019-02-14 20:12:21 -0500

    subiendo-2

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
+  const form = document.querySelector("form");
 
-};
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const input = document.querySelector("input");
+    alert(`Hola ${input.value}`)
+  })
+});
